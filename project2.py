1992b1e21b49d741c7cd8a4d7fd04cda9a9706f8
yxu453 <yxu453@gatech.edu>
2019-06-29 16:20:33 -0700

    Refactor 11

--- project2.py
+++ project2.py
@@ -1,8 +1,6 @@
 import torch.nn as nn
 import torch
 import gym
-import numpy as np
-from time import time
 import pandas as pd
 from collections import deque
 import random
@@ -42,7 +40,7 @@ class FeedForwardNetwork(nn.Module):
 
 
 class QLearner:
-    def __init__(self, env, model, epsilon_decay=0.998, epsilon_min=0.1, gamma=0.99):
+    def __init__(self, env, model, epsilon_decay, epsilon_min, gamma):
         self.env = env
         self.model = model
         self.epsilon_decay = epsilon_decay
@@ -75,36 +73,28 @@ class QLearner:
 
         return states, q_vals
 
-    def run(self,
-            num_episodes=2000,
-            current_epsilon=1.0,
-            replay_memory=2 ** 16,
-            replay_sample_size=32,
-            training_start_memory_size=64,
-            most_recent_count=100,
-            logging=True,
-            train=True,
-            render=False):
+    def train(self,
+              num_episodes,
+              current_epsilon,
+              replay_memory,
+              replay_sample_size,
+              training_start_memory_size,
+              most_recent_count):
 
+
+        # initial set up
         logs = None
-        if type(logging) is pd.DataFrame:
-            logs = logging
-            logging = True
-
         env, model = self.env, self.model
-
-        if train:
-            replay_memory = deque([], maxlen=replay_memory)
+        replay_memory = deque([], maxlen=replay_memory)
         recent_rewards = [0] * most_recent_count
 
-        for episode_idx in range(num_episodes):
+        for episode_index in range(num_episodes):
 
             total_reward = 0
             current_state = env.reset()
             done = False
             action_count = 0
 
-            train_start_time = time()
             while not done:
 
                 # epsilon greedy strategy to select the actions to take
@@ -116,50 +106,38 @@ class QLearner:
                 # interact with the environment
                 next_state, reward, done, info = env.step(act)
 
-                if render:
-                    env.render();
-                if train:
-                    replay_memory.append((current_state, act, reward, next_state, done))
-                    if len(replay_memory) >= training_start_memory_size:
-                        states, qvalues = self.get_experience_replay_data(replay_memory, replay_sample_size)
-                        loss = float(self.model.train(states, qvalues))
-                    else:
-                        loss = 0
+                # add to the replay memory
+                replay_memory.append((current_state, act, reward, next_state, done))
+                if len(replay_memory) >= training_start_memory_size:
+                    states, qvalues = self.get_experience_replay_data(replay_memory, replay_sample_size)
+                    loss = float(self.model.train(states, qvalues))
 
+                # update state, reward, action count
                 current_state = next_state
                 total_reward += reward
                 action_count += 1
-            curr_time_use = time() - train_start_time
 
+            # update current_epsilon
             if current_epsilon > self.epsilon_end:
                 current_epsilon *= self.epsilon_decay
 
-            recent_rewards[episode_idx % most_recent_count] = total_reward
+            # record the most recent episode reward
+            recent_rewards[episode_index % most_recent_count] = total_reward
+            if episode_index >= 100:
+                most_rent_mean_reward = sum(recent_rewards) / most_recent_count
+            else:
+                most_rent_mean_reward = sum(recent_rewards) / (episode_index + 1)
 
-            most_rent_mean_reward = sum(recent_rewards) / most_recent_count
+            log_entry = (episode_index, total_reward, most_rent_mean_reward, loss, current_epsilon)
+            if logs is None:
+                logs = pd.DataFrame(columns=("Episode", "Total Reward", "Mean Reward", "Train Loss", "Epsilon"))
+            logs.loc[episode_index] = log_entry
+            print("Episode:{}, Current Reward: {}, Mean Reward: {:.3f}, Train Loss: {:.3f}, Epsilon: {:.5f}".format(*log_entry))
 
-            log_entry = (
-                episode_idx, total_reward, most_rent_mean_reward, loss, current_epsilon, action_count, curr_time_use)
-            if logging:
-                if logs is None:
-                    logs = pd.DataFrame(columns=("Episode", "Total Reward", "Mean Reward", "Train Loss", "Epsilon", "Actions", "Training Time"))
-                logs.loc[episode_idx] = log_entry
-            print("Episode:{}, Total Reward: {}, Mean Reward: {:.2f}, Train Loss: {:.3f}, Epsilon: {:.5f}, Actions: {}, Training Time: {:.3f}".format(*log_entry))
+        # save the log to csv
+        logs.to_csv("result_gamma{}_alpha{}.csv".format(gamma, alpha), index=False)
 
-        logs["Episode"] = logs["Episode"].astype(int)
-        logs["Actions"] = logs["Actions"].astype(int)
-        return most_rent_mean_reward, logs
-
-    def train(self, episode_count=2500, epsilon_start=1.0, replay_memory=2 ** 16, replay_sample_size=32,
-              training_start_memory_size=64, mean_reward_recency=100,
-              logging=True, render=False):
-        return self.run(num_episodes=episode_count,
-                        current_epsilon=epsilon_start,
-                        replay_memory=replay_memory,
-                        replay_sample_size=replay_sample_size,
-                        training_start_memory_size=max(replay_sample_size, training_start_memory_size),
-                        most_recent_count=mean_reward_recency,
-                        logging=logging, train=True, render=render)
+        return
 
     def test(self, start_episode_idx=0, episode_count=100, logging=True, continued_learning=True, replay_memory=2 ** 16,
              replay_sample_size=32, render=False):
@@ -173,17 +151,17 @@ class QLearner:
 
 if __name__ == "__main__":
     env = gym.make('LunarLander-v2')
-    hidden_layer_dimensions=[128, 64]
-    training_episode_count=2000
-    alpha=1e-4
-    gamma=0.99
-    epsilon_start=1.0
-    epsilon_decay=0.998
-    epsilon_min=0.0
-    replay_memory_size=2 ** 16
-    replay_sample_size=32
-    training_start_memory_size=64
-    most_recent_count=100
+    hidden_layer_dimensions = [128, 64]
+    training_episode_count = 200
+    alpha = 1e-4
+    gamma = 0.99
+    epsilon_start = 1.0
+    epsilon_decay = 0.998
+    epsilon_min = 0.0
+    replay_memory_size = 65536
+    replay_sample_size = 32
+    training_start_memory_size = 64
+    most_recent_count = 100
 
     # set the dimensions of the model
     # the input size is the dimension of the observation, the output size is the dimension of the action space
@@ -191,7 +169,7 @@ if __name__ == "__main__":
 
     ffw = FeedForwardNetwork(dimensions=dimensions,
                              loss_fun=nnF.mse_loss,
-                             optimizer=lambda mode_paras: torch.optim.Adam(mode_paras, lr=alpha))
+                             optimizer=lambda parameters: torch.optim.Adam(parameters, lr=alpha))
 
     lunar_lander = QLearner(env=env,
                             model=ffw,
@@ -199,9 +177,9 @@ if __name__ == "__main__":
                             epsilon_min=epsilon_min,
                             gamma=gamma)
 
-    mean_reward, logs = lunar_lander.train(episode_count=training_episode_count,
-                                           epsilon_start=epsilon_start,
-                                           replay_memory=replay_memory_size,
-                                           replay_sample_size=replay_sample_size,
-                                           training_start_memory_size=training_start_memory_size,
-                                           mean_reward_recency=most_recent_count)
+    lunar_lander.train(num_episodes=training_episode_count,
+                       current_epsilon=epsilon_start,
+                       replay_memory=replay_memory_size,
+                       replay_sample_size=replay_sample_size,
+                       training_start_memory_size=max(replay_sample_size, training_start_memory_size),
+                       most_recent_count=most_recent_count)
